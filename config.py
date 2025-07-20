57227007a506e7a446dbf70ed8036145b90fab68
Mahendra 'Logan' Choudhary <choudharymahendra183@gmail.com>
2025-07-20 23:50:44 +0100

    0.7

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "openai_api_key")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
 EMBEDDING_MODEL = "text-embedding-3-small"
 LLM_MODEL = "gpt-4.1-nano-2025-04-14"
 
 FAISS_DIR = "faiss_nhs_sections"
-DATA_DIR = "data"
+PDF_DIR = "data"
