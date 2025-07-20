0dfbcedc829dc0a4cf6d3be85b4bdc9a56da62e8
Mahendra 'Logan' Choudhary <choudharymahendra183@gmail.com>
2025-07-21 00:08:12 +0100

    0.8

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
+import streamlit as st
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 EMBEDDING_MODEL = "text-embedding-3-small"
 LLM_MODEL = "gpt-4.1-nano-2025-04-14"
 
