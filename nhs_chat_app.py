2c14a4c437556cff88d4908dfbae0727964261fe
Mahendra 'Logan' Choudhary <choudharymahendra183@gmail.com>
2026-04-27 01:11:46 +0530

    Fix 0.24

--- nhs_chat_app.py
+++ nhs_chat_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 
 from services.rag_service import RAGService
-# from config import DATA_DIR
+from config import DATA_DIR
 
 # os.environ["OPENAI_API_KEY"] = st.secrets["openai"]["api_key"]
 
