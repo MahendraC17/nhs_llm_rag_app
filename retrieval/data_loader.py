1d87e2634b13b40e257f4b4c67e8f903ae141418
Mahendra 'Logan' Choudhary <choudharymahendra183@gmail.com>
2026-05-04 22:32:25 +0530

    cache fix at st level

--- retrieval/data_loader.py
+++ retrieval/data_loader.py
@@ -1,4 +1,5 @@
 import nltk
+import streamlit as st
 
 from data_chunking import load_and_chunk_pdfs
 from config import FAISS_DIR
@@ -9,14 +10,9 @@ except LookupError:
     nltk.download("punkt")
     nltk.download("punkt_tab")
 
-_docs_cache = None
-
+@st.cache_resource
 def get_documents():
-    global _docs_cache
-
-    if _docs_cache is None:
-        _docs_cache = load_and_chunk_pdfs()
-        print(f"FAISS index path: {FAISS_DIR}")
-        print("Loading documents...")
-
-    return _docs_cache
+    docs = load_and_chunk_pdfs()
+    print(f"FAISS index path: {FAISS_DIR}")
+    print("Loading documents...")
+    return docs
